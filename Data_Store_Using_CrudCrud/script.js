3a935b954ea67f0eb9841db6ec50800cf9e9b699
MD AURANGZEB <115891944+Imam-007@users.noreply.github.com>
2023-12-09 11:53:49 +0530

    Update script.js

--- Data_Store_Using_CrudCrud/script.js
+++ Data_Store_Using_CrudCrud/script.js
@@ -1,129 +1,110 @@
-function saveToCrud(event){
-  event.preventDefault();
-
-  var itemName = event.target.itemName.value;
-  var desc = event.target.desc.value;
-  var price =event.target.price.value;
-  var quantity = event.target.quantity.value;
-
-  const obj ={
-      itemName,
-      desc,
-      price,
-      quantity
-  }
-
-
-
-  axios.post('https://crudcrud.com/api/e35e8c918a4a4de4a171daa7d24eafe7/UserData',obj)
-  .then(res=>{
-      showUserOnScreen(res.data);
-  })
-  .catch(err=>{
-      document.body.innerHTML = document.body.innerHTML+"<h4>Something went wrong</h4>"
-  })
-
-
+async function saveToCrud(event){
+    event.preventDefault();
+  
+    var itemName = event.target.itemName.value;
+    var desc = event.target.desc.value;
+    var price =event.target.price.value;
+    var quantity = event.target.quantity.value;
+  
+    const obj ={
+        itemName,
+        desc,
+        price,
+        quantity
+    } 
+  
+    const res=await axios.post('https://crudcrud.com/api/96990cc404204b8f95fa264872ffac2a/UserData',obj)
+    showUserOnScreen(res.data);   
 }
-//show data after refereshing the page
-window.addEventListener('DOMContentLoaded',()=>{
-  axios.get('https://crudcrud.com/api/e35e8c918a4a4de4a171daa7d24eafe7/UserData')
-  .then((res)=>{
-      for(var i=0;i<res.data.length;i++){
-          showUserOnScreen(res.data[i]);
-      }
-  })
-  .catch(err=>console.log(err))
-})
-
-function showUserOnScreen(obj){
-  const parentElement = document.getElementById('listOfItems');
-  const childElement = document.createElement('li');
-
-  //Add text to li
-childElement.textContent = obj.itemName + "-" + obj.desc +"-"+ obj.price+ "-" + obj.quantity; 
-
-  //create buy button
-  const btn1 = document.createElement('button');
-  const btn2 = document.createElement('button');
-  const btn3 = document.createElement('button');
-
-
-  btn1.textContent ='buy1'
-  btn2.textContent='buy2'
-  btn3.textContent ='buy3'
-
-  btn1.onclick=()=>{
-     const id =obj._id;
-     const updatedValues={
-      itemName:obj.itemName,
-      desc:obj.desc,
-      price:obj.price,
-      quantity:obj.quantity-1
-     }
-     console.log(updatedValues);
-     axios.put(`https://crudcrud.com/api/e35e8c918a4a4de4a171daa7d24eafe7/UserData/${id}`,updatedValues)
-     .then(()=>{
-      obj.quantity=obj.quantity-1;
-      childElement.textContent = obj.itemName + "-" + obj.desc +"-"+ obj.price+ "-" + obj.quantity;
-      childElement.appendChild(btn1);
-      childElement.appendChild(btn2);
-      childElement.appendChild(btn3); 
-     })
-     .catch(err=>console.log(err))
-   
-
-  }
-
-  btn2.onclick = ()=>{
-    const id =obj._id;
-     const updatedValues={
-      itemName:obj.itemName,
-      desc:obj.desc,
-      price:obj.price,
-      quantity:obj.quantity-2
-     }
-     console.log(updatedValues);
-     axios.put(`https://crudcrud.com/api/e35e8c918a4a4de4a171daa7d24eafe7/UserData/${id}`,updatedValues)
-     .then(()=>{
-      obj.quantity=obj.quantity-2;
-      childElement.textContent = obj.itemName + "-" + obj.desc +"-"+ obj.price+ "-" + obj.quantity;
-      childElement.appendChild(btn1);
-      childElement.appendChild(btn2);
-      childElement.appendChild(btn3); 
-     })
-     .catch(err=>console.log(err))
-  }
-
-  btn3.onclick=()=>{
-    const id =obj._id;
-    const updatedValues={
-     itemName:obj.itemName,
-     desc:obj.desc,
-     price:obj.price,
-     quantity:obj.quantity-3
+window.addEventListener('DOMContentLoaded',async()=>{
+    const res=await axios.get(`https://crudcrud.com/api/96990cc404204b8f95fa264872ffac2a/UserData`);
+    for(let i=0;i<res.data.length;i++){
+        showUserOnScreen(res.data[i]);
     }
-    console.log(updatedValues);
-    axios.put(`https://crudcrud.com/api/e35e8c918a4a4de4a171daa7d24eafe7/UserData/${id}`,updatedValues)
-    .then(()=>{
-     obj.quantity=obj.quantity-3;
-     childElement.textContent = obj.itemName + "-" + obj.desc +"-"+ obj.price+ "-" + obj.quantity;
-     childElement.appendChild(btn1);
-     childElement.appendChild(btn2);
-     childElement.appendChild(btn3); 
-    })
-    .catch(err=>console.log(err))
-  }
+   })
+  
+function showUserOnScreen(obj){
+    const parentElement = document.getElementById('listOfItems');
+    const childElement = document.createElement('li');
+  
+    //Add text to li
+  childElement.textContent = obj.itemName + "-" + obj.desc +"-"+ obj.price+ "-" + obj.quantity; 
+  
+    //create buy button
+    const btn1 = document.createElement('button');
+    const btn2 = document.createElement('button');
+    const btn3 = document.createElement('button');
+  
+  
+    btn1.textContent ='buy1'
+    btn2.textContent='buy2'
+    btn3.textContent ='buy3'
+  
+    btn1.onclick=async()=>{
+       const id =obj._id;
+       const updatedValues={
+        itemName:obj.itemName,
+        desc:obj.desc,
+        price:obj.price,
+        quantity:obj.quantity-1
+       }
+       console.log(updatedValues);
 
-  //appned btn to childElement
-  childElement.appendChild(btn1);
-  childElement.appendChild(btn2);
-  childElement.appendChild(btn3);
+        await axios.put(`https://crudcrud.com/api/96990cc404204b8f95fa264872ffac2a/UserData/${id}`,updatedValues);
+        obj.quantity=obj.quantity-1;
+        childElement.textContent = obj.itemName + "-" + obj.desc +"-"+ obj.price+ "-" + obj.quantity;
+        childElement.appendChild(btn1);
+        childElement.appendChild(btn2);
+        childElement.appendChild(btn3);
+     
+  
+    }
+  
+    btn2.onclick =async ()=>{
+      const id =obj._id;
+       const updatedValues={
+        itemName:obj.itemName,
+        desc:obj.desc,
+        price:obj.price,
+        quantity:obj.quantity-2
+       }
+       console.log(updatedValues);
 
+       await axios.put(`https://crudcrud.com/api/96990cc404204b8f95fa264872ffac2a/UserData/${id}`,updatedValues)
+       
+        obj.quantity=obj.quantity-2;
+        childElement.textContent = obj.itemName + "-" + obj.desc +"-"+ obj.price+ "-" + obj.quantity;
+        childElement.appendChild(btn1);
+        childElement.appendChild(btn2);
+        childElement.appendChild(btn3); 
 
-
- //Append li to ul
-parentElement.appendChild(childElement)
-
-
+    }
+  
+    btn3.onclick=async()=>{
+      const id =obj._id;
+      const updatedValues={
+       itemName:obj.itemName,
+       desc:obj.desc,
+       price:obj.price,
+       quantity:obj.quantity-3
+      }
+      console.log(updatedValues);
+      await axios.put(`https://crudcrud.com/api/96990cc404204b8f95fa264872ffac2a/UserData/${id}`,updatedValues)
+       obj.quantity=obj.quantity-3;
+       childElement.textContent = obj.itemName + "-" + obj.desc +"-"+ obj.price+ "-" + obj.quantity;
+       childElement.appendChild(btn1);
+       childElement.appendChild(btn2);
+       childElement.appendChild(btn3); 
+    }
+  
+    //appned btn to childElement
+    childElement.appendChild(btn1);
+    childElement.appendChild(btn2);
+    childElement.appendChild(btn3);
+  
+  
+   //Append li to ul
+  parentElement.appendChild(childElement)
+  
+  
 }
